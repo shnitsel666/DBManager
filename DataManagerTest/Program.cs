2a05258ff11894ecab25f34f400072ee7e754f0e
Artyom Shtirner <artyom.lezhnev@mail.ru>
2022-12-12 22:24:08 +0600

    Update Program.cs

--- DataManagerTest/Program.cs
+++ DataManagerTest/Program.cs
@@ -1,26 +1,37 @@
-﻿using DataManagerTest.TestModels;
-using System.Linq;
-using System.IO;
-using Database.DataManager;
-
 namespace DataManagerTest
 {
-    ///<summary>
-    ///Консольное приложение для тестирования DataManager
+    using DatabaseManager;
+
+    /// <summary>
+    /// Console Application for DBManager testing.
     /// </summary>
     class Program
     {
-        private static string _connectionString = "Data Source=0.0.0.0,1234; Initial Catalog=TestCatalog;Integrated Security=true;";
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            using(DataManager manager = new DataManager(_connectionString))
-            {
-                List<List<string>> str = new List<List<string>>();
-                for(int i = 0; i < 10; i++)
-                {
-                    str.Add(new List<string>());
-                }
-            }
+            DBManagerPool _DBManagerPool = new();
+            _DBManagerPool.DefaultManager.ExecuteNonQuery("SELECT 1 = 1");
+        }
+    }
+
+    public class DBManagerPool
+    {
+        private readonly string _defaultConnectionString = "Data Source=0.0.0.0,1234; Initial Catalog=TestCatalog;Integrated Security=true;";
+
+        private readonly string _1CConnectionString = "Data Source=0.0.0.0,1234; Initial Catalog=TestCatalog;Integrated Security=true;";
+
+        private readonly DBPool dbPool1;
+
+        private readonly DBPool dbPool2;
+
+        public DBManager DefaultManager => dbPool1.Manager;
+
+        public DBManager _1CManager => dbPool2.Manager;
+
+        public DBManagerPool()
+        {
+            dbPool1 = new DBPool(_defaultConnectionString, 10);
+            dbPool2 = new DBPool(_1CConnectionString, 10);
         }
     }
 }
